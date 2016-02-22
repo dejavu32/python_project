3f12175d134db27f9a49963fe4d3d00bf38ac206
dejavu32 <carrotship@naver.com>
2016-02-22 17:56:18 +0900

    from konlpy.tag import Hannanum  사용버젼 commit

--- python project/tos_crawling.py
+++ python project/tos_crawling.py
@@ -5,20 +5,17 @@ from bs4 import BeautifulSoup as bs
 f = open('./board.txt', 'w')
 
 #################################### 건의 게시판 본문 가져오기 테스트
-result = requests.get('http://tos.nexon.com/community/suggest/view.aspx?n4ArticleSN=27')     # 건의게시판
-# result = requests.get('http://tos.nexon.com/community/free/view.aspx?n4ArticleSN=106501')     # 자유게시판
-re_soup = bs(result.text,'html.parser')
-
-words = re_soup.findAll('div',{'class':'viewContents'})[0].text.split()
-word = re_soup.findAll('div',{'class':'viewContents'})[0].text
+# result = requests.get('http://tos.nexon.com/community/suggest/view.aspx?n4ArticleSN=27')     # 건의게시판
+# # result = requests.get('http://tos.nexon.com/community/free/view.aspx?n4ArticleSN=106501')     # 자유게시판
+# re_soup = bs(result.text,'html.parser')
+#
+# words = re_soup.findAll('div',{'class':'viewContents'})[0].text.split()
+# word = re_soup.findAll('div',{'class':'viewContents'})[0].text
 
 
 #################################### 건의 게시판 본문 가져오기
-num = range(1, 100)
-for n in num:
-    url = 'http://tos.nexon.com/community/suggest/view.aspx?n4ArticleSN='+ str(n)
-    print url
-    result = requests.get(url)
+
+def get_url_text(result):
 
     try:
         if result.status_code == 200:
@@ -37,15 +34,63 @@ for n in num:
             #          '본문 : ' + word.encode('utf-8').strip() + '\n\n']
 
             lines = [str(n)+'\t', title.encode('utf-8').strip()+ '\t', date[0].text.encode('utf-8').strip() + '\t', word.encode('utf-8').strip() + '\n']
-
-            print lines
-            f.writelines(lines)
+            # print lines
+            # f.writelines(lines)
+            board_main = word.encode('utf-8').strip()        # 게시판 본문 내용
+            # print board_main
+            return board_main
 
     except IndexError:
-        print n , '글없음'
-
-
+        # print n , '글없음'
+        return '글없음'
 
 f.close()
+
+# 샘플로 여러라인 출력하기
+# num = range(1, 100)
+# for n in num:
+#     url = 'http://tos.nexon.com/community/suggest/view.aspx?n4ArticleSN='+ str(n)
+#     # print url             # 테스트용 url 출력
+#     result = requests.get(url)
+
+from collections import Counter
+import urllib
+import random
+import webbrowser
+from lxml import html
+# import pytagcloud
+import sys
+
+r = lambda: random.randint(0,255)
+color = lambda: (r(), r(), r())
+
+
+from konlpy.tag import Hannanum    # 한국어 형태소 분석 모듈
+
+def get_tags(text, ntags=50, multiplier=10):
+    h = Hannanum()
+    nouns = h.nouns(text)
+    count = Counter(nouns)
+    return [{ 'color': color(), 'tag': n, 'size': c*multiplier }\
+                for n, c in count.most_common(ntags)]
+    # return nouns
+
+
+# import webbrowser
+# import pytagcloud           # word cloud 그리기
+#
+# def draw_cloud(tags, filename, fontname='Noto Sans CJK', size=(800, 600)):
+#     pytagcloud.create_tag_image(tags, filename, fontname=fontname, size=size)
+#     webbrowser.open(filename)
+#
+#
+# draw_cloud(tags, 'wordcloud.png')
+
+url = 'http://tos.nexon.com/community/suggest/view.aspx?n4ArticleSN='+ str(100)
+print url             # 테스트용 url 출력
+result = requests.get(url)
+tags = get_tags(result)         # 입렵받은 url의 본문 내용을 tags로 나누어주는
+print(tags)
+# draw_cloud(tags, 'wordcloud.png')
 
 
